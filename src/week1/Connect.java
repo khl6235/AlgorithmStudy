81cdd7c9fc440e1150dd8d0de1fc8ca8af563517
Haely <khl6235@naver.com>
2020-03-18 18:27:48 +0900

    BOJ #11724-DFS

---
+++ src/week1/Connect.java
@@ -0,0 +1,61 @@
+package week1;
+
+/*
+BOJ #11724
+[DFS] 간선 다 집어넣어놓고, 방문하지 않은 정점 돌면서 한 세트 끝나면 cnt++ 시킴)
+*/
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Connect {
+	
+	static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+	static boolean[] visited;
+	static int cnt;
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] str = br.readLine().split(" ");
+		
+		int N = Integer.parseInt(str[0]);
+		int M = Integer.parseInt(str[1]);
+		
+		for(int i = 0; i <= N; i++) {
+			graph.add(new ArrayList<>());
+		}
+		visited = new boolean[N+1];
+		
+		for(int i = 0; i < M; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			int u = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
+			graph.get(u).add(v);
+			graph.get(v).add(u);
+			
+		}
+		
+		for(int i = 1; i <= N; i++) {
+			if(!visited[i]) {
+				dfs(i);
+				cnt++;
+			}
+		}
+		System.out.println(cnt);
+
+	}
+	public static void dfs(int i){
+		visited[i] = true;
+		for(int k : graph.get(i)) {
+			if(!visited[k]) {
+				dfs(k);
+			}
+		}
+		
+	}
+}
